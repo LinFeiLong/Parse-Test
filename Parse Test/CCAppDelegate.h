145a8f9c16353295948d98bd1fcee3c847a62383
Fernand LIME <flime@e3r9p10.42.fr>
2014-07-20 07:22:56 +0200

    Initial Commit

---
+++ Parse Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Parse Test
+//
+//  Created by Fernand LIME on 20/07/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Parse Test/CCViewController.m
@@ -0,0 +1,29 @@
+//
+//  CCViewController.m
+//  Parse Test
+//
+//  Created by Fernand LIME on 20/07/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCViewController ()
+
+@end
+
+@implementation CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
